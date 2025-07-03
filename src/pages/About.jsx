35ccdc261496e692353caececfa6e2620256860c
dev-manish301 <rajzmanish94@gmail.com>
2025-07-03 11:46:11 +0530

    try to create home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="px-20 py-10">
+      <h1 className="text-3xl font-medium">About section</h1>
+      <p className="text-zinc-500">
+        let's try to we know each other
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
